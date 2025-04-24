da42eb7382a93969bd61fb59f305ed7cb65e7c54
liubrian267 <liubrian267@gmail.com>
2025-04-23 18:57:17 -0700

    changed bullet points

--- src/app/page.js
+++ src/app/page.js
@@ -24,11 +24,12 @@ const page = () => {
       <div className="mt-10 text-5xl font-bold">
         Experience
         <hr className="mt-2 border border-#F5F5F5"></hr>
-        <ul className="text-2xl font-normal">
-          <li> - Software Engineering Intern at San Jose State University </li>
-          <li> - Software Engineering Intern at CodeDay Labs </li>
-          <li> - Tech Lead at Google Developer Student Club</li>
+        <ul className="list-disc list-inside text-2xl font-normal">
+          <li>Software Engineering Intern at San Jose State University</li>
+          <li>Software Engineering Intern at CodeDay Labs</li>
+          <li>Tech Lead at Google Developer Student Club</li>
         </ul>
+
       </div>
       <div className="mt-10 text-5xl font-bold">
         Skills
@@ -43,17 +44,17 @@ const page = () => {
       <div className="mt-10 text-5xl font-bold">
         What I've been up to
         <hr className="mt-2 border border-#F5F5F5"></hr>
-        <ul className="text-2xl font-normal">
+        <ul className="text-2xl font-normal list-disc list-inside">
           <li>
             <a
               href="https://github.com/liubrian267/Bruin-Brawlers"
               className="transition-colors duration-200 hover:text-blue-500 hover:underline"
             >
-              - Learning automation to refresh data for my Brawl Stars
+              Learning automation to refresh data for my Brawl Stars
               statistics application
             </a>
           </li>
-          <li> - Self-studying machine learning topics </li>
+          <li> Self-studying machine learning topics </li>
         </ul>
       </div>
     </div>

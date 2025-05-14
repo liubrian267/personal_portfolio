d96437413e3537acc7a985afa4d81c10cddd6f50
liubrian267 <liubrian267@gmail.com>
2025-05-14 14:44:41 -0700

    fixing navbar mobile view

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,8 +3,8 @@ import Link from 'next/link'
 import Image from 'next/image'
 export const Navbar = () => {
   return (
-    <header className="p-4 border border-#F5F5F5 rounded-bl-lg rounded-br-lg">
-  <div className="w-full flex justify-between items-center">
+    <header className="py-4 px-6 sm:px-8 border border-[#F5F5F5] rounded-bl-lg rounded-br-lg">
+  <div className="w-full flex items-center justify-between gap-x-6 sm:gap-x-8 md:gap-x-12">
     <nav className="flex items-center space-x-6">
       <Link href="/" className="text-lg font-medium hover:text-gray-300 pl-8">
         Brian Liu
@@ -16,7 +16,7 @@ export const Navbar = () => {
         Projects
       </Link>
     </nav>
-    <div className="flex items-center space-x-6">
+    <div className="flex items-center space-x-6 flex-shrink-0">
       <Link href="/Brian_Liu_Resume.pdf" className="hover:text-gray-300" aria-label="Resume">
       <Image
           src="/whiteresume.svg"
